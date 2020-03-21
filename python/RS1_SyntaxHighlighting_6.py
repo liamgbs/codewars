b4116ff29e240b2d76e202c90b273b23a54cbd0b
liamgbs <liam.gibbings@missionlabs.co.uk>
2020-03-21 21:40:18 +0000

    rs1 syntax highlighting python

---
+++ python/RS1_SyntaxHighlighting_6.py
@@ -0,0 +1,11 @@
+import re;
+
+def highlight(code):
+    code = re.sub(r"(F+)", r'<span style="color: pink">\1</span>', code)
+    code = re.sub(r"(L+)", r'<span style="color: red">\1</span>', code)
+    code = re.sub(r"(R+)", r'<span style="color: green">\1</span>', code)
+    code = re.sub(r"(\d+)", r'<span style="color: orange">\1</span>', code)
+    return code
+
+
+print(highlight("FFF"))
